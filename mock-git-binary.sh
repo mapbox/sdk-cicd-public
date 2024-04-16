604d9c3a41cb6a925f621287d51642be8bb2850b
Sergey Akentev <sergey.akentev@mapbox.com>
2024-04-16 12:15:31 +0300

    Support Xcode's git path mocker

--- mock-git-binary.sh
+++ mock-git-binary.sh
@@ -8,11 +8,25 @@ else
     TEMP_DIR="/tmp"
 fi
 BACKUP_GIT_PATH="${TEMP_DIR%/}/git_bak"
+
+# Backup and mock the primary Git binary
 sudo mv "${ORIGINAL_GIT_PATH}" "${BACKUP_GIT_PATH}"
 # shellcheck disable=SC2016
 echo -e '#!/bin/sh\necho "MOCKED COMMAND: $0 $@"' | sudo tee "${ORIGINAL_GIT_PATH}"
 sudo chmod +x "${ORIGINAL_GIT_PATH}"
-# shellcheck disable=SC2086
-echo "export ORIGINAL_GIT_PATH=\"${ORIGINAL_GIT_PATH}\"" >> $BASH_ENV
-# shellcheck disable=SC2086
-echo "export BACKUP_GIT_PATH=\"${BACKUP_GIT_PATH}\"" >> $BASH_ENV
+
+# Check for macOS and mock Xcode's Git binary if it exists
+if [[ "$(uname)" == "Darwin" ]]; then
+    XCODE_GIT_PATH="$(xcode-select -p)/usr/bin/git"
+    if [[ -f "${XCODE_GIT_PATH}" ]]; then
+        BACKUP_XCODE_GIT_PATH="${TEMP_DIR%/}/xcode_git_bak"
+        sudo mv "${XCODE_GIT_PATH}" "${BACKUP_XCODE_GIT_PATH}"
+        sudo ln -s "${ORIGINAL_GIT_PATH}" "${XCODE_GIT_PATH}"
+        echo "export BACKUP_XCODE_GIT_PATH=\"${BACKUP_XCODE_GIT_PATH}\"" >> "${BASH_ENV}"
+    fi
+fi
+
+
+# Environment variables to restore original paths later
+echo "export ORIGINAL_GIT_PATH=\"${ORIGINAL_GIT_PATH}\"" >> "${BASH_ENV}"
+echo "export BACKUP_GIT_PATH=\"${BACKUP_GIT_PATH}\"" >> "${BASH_ENV}"
--- unmock-git-binary.sh
+++ unmock-git-binary.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -eox pipefail
 
+# Restore the primary Git binary
 sudo mv "${BACKUP_GIT_PATH}" "${ORIGINAL_GIT_PATH}"
-# shellcheck disable=SC2086
-echo "unset ORIGINAL_GIT_PATH" >> $BASH_ENV
-# shellcheck disable=SC2086
-echo "unset BACKUP_GIT_PATH" >> $BASH_ENV
+
+# Restore Xcode's Git binary if it was backed up
+if [[ -n "${BACKUP_XCODE_GIT_PATH}" ]]; then
+    XCODE_GIT_PATH="$(xcode-select -p)/usr/bin/git"
+    sudo mv "${BACKUP_XCODE_GIT_PATH}" "${XCODE_GIT_PATH}"
+    echo "unset BACKUP_XCODE_GIT_PATH" >> "${BASH_ENV}"
+fi
+
+# Clean up environment variables
+echo "unset ORIGINAL_GIT_PATH" >> "${BASH_ENV}"
+echo "unset BACKUP_GIT_PATH" >> "${BASH_ENV}"
